82e7b0e25aa1c734aa3a200bd6cae485087b5522
maky.madingou <maky.madingou@wizall.com>
2021-08-29 16:07:12 +0000

    update

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -45,12 +45,9 @@ const authController = {
 
   login: async (req, res) => {
     try {
-      const {
-        email,
-        password
-      } = req.body;
+      const { uid } = req.body;
       const user = await Users.findOne({
-        email: email
+        uid: uid
       }).populate([
         {
           path: "adresses",
@@ -65,32 +62,9 @@ const authController = {
           status: 400,
           message: "Cet utilisateur n'existe pas.",
         });
-
-      // if (!user.status)
-      //   return res.status(400).send({
-      //     status: 400,
-      //     message:
-      //       "Votre compte n'est pas encore activé, veuillez verifier votre adresse e-mail.",
-      //   });
-
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch)
-        return res.status(400).json({
-          status: 400,
-          message: "Mot de passe incorrect",
-        });
-
-      const payload = {
-        id: user._id,
-        name: user.username
-      };
-      // const token = jwt.sign(payload, process.env.TOKEN_SECRET, {
-      //   expiresIn: "1d",
-      // });
-
-
       res.status(200).json({
         id: user.id,
+        uid: user.uid,
         firstname: user.firstname,
         lastname: user.lastname,
         profil: user.profil,
